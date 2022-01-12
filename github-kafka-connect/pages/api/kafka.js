47ba493b96dca4a2d60f9e1dd4a98542feaf1250
enesakar <enesakar@gmail.com>
2022-01-12 15:30:32 -0800

    github webhook

--- github-kafka-connect/pages/api/kafka.js
+++ github-kafka-connect/pages/api/kafka.js
@@ -1,6 +1,17 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const address = "https://full-mantis-14189-us1-rest-kafka.upstash.io"
+const user = 'ZnVsbC1tYW50aXMtMTFdjveYHBV9Jjzow03SnUtRQ'
+const pass = '4-R-fmtoalXnoeu9TjQBOOL4njfSKwEsE10Yrrq5_yAq4TPGd9c6JbqfQ=='
+const auth = Buffer.from(`${user}:${pass}`).toString('base64')
+const topic = 'github-events'
 
-export default function handler(req, res) {
-  console.log(req)
-  res.status(200).json({ name: 'kafka success2' })
+export default async function handler(req, res) {
+  let eventData = JSON.stringify(req.body);
+  let x = await fetch(`${address}/produce/${topic}`, {
+    method: 'POST',
+    headers: {'Authorization': `Basic ${auth}`},
+    body: JSON.stringify({"value" : eventData})
+  })
+  const response = await x.json();
+  console.log(response)
+  res.status(200).json({ name: 'kafka success-6' })
 }
