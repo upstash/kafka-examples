753bd1f8722a90059e4e8c8f290f0757402e9e15
enesakar <enesakar@gmail.com>
2022-01-12 13:03:15 -0800

    init

---
+++ github-kafka-connect/pages/api/kafka.js
@@ -0,0 +1,6 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  console.log(req)
+  res.status(200).json({ name: 'kafka success' })
+}
