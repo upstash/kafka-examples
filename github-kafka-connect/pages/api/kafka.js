a5de831afd7b3f0363c90f61f7d6fc140c938445
enesakar <enesakar@gmail.com>
2022-01-13 12:09:16 -0800

    fff

--- github-kafka-connect/pages/api/kafka.js
+++ github-kafka-connect/pages/api/kafka.js
@@ -1,11 +1,10 @@
-const address = "https://full-mantis-14189-us1-rest-kafka.upstash.io"
-const user = 'ZnVsbC1tYW50aXMtMTFdjveYHBV9Jjzow03SnUtRQ'
-const pass = '4-R-fmtoalXnoeu9TjQBOOL4njfSKwEsE10Yrrq5_yAq4TPGd9c6JbqfQ=='
+const address = "https://full-mantis-14289-us1-kafka.upstash.io:9092"
+const user = 'ZnVsbC1tYW50aXMtMTQyODkkimaEsuUsiT9TGk3OFdjveYHBV9Jjzow03SnUtRQ'
+const pass = '4-R-fmtoalXnoeu9TjQBOOL4njfSKwEsE10YvHMiW63hFljqUrrq5_yAq4TPGd9c6JbqfQ=='
 const auth = Buffer.from(`${user}:${pass}`).toString('base64')
 const topic = 'github-events'
 
 export default async function handler(req, res) {
-  console.log(req.query)
   let eventData = JSON.stringify(req.body);
   let x = await fetch(`${address}/produce/${topic}`, {
     method: 'POST',
@@ -14,5 +13,5 @@ export default async function handler(req, res) {
   })
   const response = await x.json();
   console.log(response)
-  res.status(200).json({ name: 'kafka success-6' })
+  res.status(200).json({ name: 'kafka success-7' })
 }
